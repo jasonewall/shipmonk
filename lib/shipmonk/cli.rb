276099281bc2066179ece49fd6b0e7678db10cf9
Jason Wall <javajo@gmail.com>
2015-02-18 09:51:54 -0700

    proper help desc formatting

--- lib/shipmonk/cli.rb
+++ lib/shipmonk/cli.rb
@@ -2,12 +2,12 @@ require 'thor'
 
 module Shipmonk
   class CLI < Thor
-    desc 'Create a new Shipmonk site.', 'Create a new Shipmonk site.'
+    desc 'new <name>', 'Create a new Shipmonk site'
     def new
       puts 'creating a new site'
     end
 
-    desc 'Start up a local server that hosts your site.', 'OMG HI'
+    desc 'server', 'Start up a local server that hosts your site.'
 
     def server
       puts 'OMG SERVER TIMEZ!'
